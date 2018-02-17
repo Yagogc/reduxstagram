862c8e2277ef3bd2199c7de7fbb1138dd33f509f
Yago <jagogc@gmail.com>
2018-02-17 18:30:33 +0000

    Adding Route

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Single = React.createClass({
+	render() {
+		return (
+			<div className="single-photo">
+				I'm single component
+			</div>
+		)
+	}
+})
+
+export default Single;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import css from './styles/style.styl'
 
 import Main from './components/Main'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
 
+const router = (
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}>
+			<IndexRoute component={PhotoGrid}></IndexRoute>
+			<Route path="/view/:postid" component={Single}></Route>
+		</Route>
+	</Router>
+)
 
-render(<Main/>, document.getElementById('root'))
+render(router, document.getElementById('root'))
