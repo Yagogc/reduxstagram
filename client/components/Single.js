548bd4899add76ef72d3539a6c9c919c64be5430
Yago <jagogc@gmail.com>
2018-02-17 22:26:58 +0000

    Updating Comment State in the Store

--- client/components/Single.js
+++ client/components/Single.js
@@ -15,7 +15,9 @@ const Single = React.createClass({
 		return (
 			<div className="single-photo">
 				<Photo i={i} post={post} {...this.props} />
-				<Comments postComments={postComments} />
+				<Comments
+					postComments={postComments}
+					{...this.props} />
 			</div>
 		)
 	}
